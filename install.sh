478119c02f41b5ff27f2d1e3fd78b682f343cd43
ShadXo <gold_guver@hotmail.com>
2021-02-09 21:27:46 +0100

    Update

--- install.sh
+++ install.sh
@@ -740,7 +740,7 @@ function source_config() {
         configure_firewall
         create_mn_configuration
         create_control_configuration
-        create_systemd_configuration2
+        create_systemd_configuration
     fi
     set_permissions
     cleanup_after
