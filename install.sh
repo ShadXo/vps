730b3eaf9bce681fb62b438a6b397db31e0fc300
ShadXo <gold_guver@hotmail.com>
2021-02-10 12:39:24 +0100

    Update

--- install.sh
+++ install.sh
@@ -932,7 +932,7 @@ function get_public_ip(){
    then
      EXTERNALIP[${NUM}]=$( timeout --signal=SIGKILL 10s wget -6qO- -T 10 -t 2 -o- "--bind-address=${BIND[${NUM}]}" http://v6.ident.me )
    else
-     EXTERNALIP[${NUM}]=$( timeout --signal=SIGKILL 10s wget -4qO- -T 10 -t 2 -o- "--bind-address=${BIND[${NUM}]}" http://ipinfo.io/ip )
+     EXTERNALIP[${NUM}]=$( wget -4qO- -T 10 -t 2 -o- "--bind-address=${BIND[${NUM}]}" http://ipinfo.io/ip )
    fi
 
    # See if public IP was found.
